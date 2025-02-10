d3662f40a0541b03b3df7e93c4cfb876a56b24c2
Brysyn Goodson <goodsonb@oregonstate.edu>
2025-02-10 11:12:23 -0800

    Made two new files Data_Setup.R and Master_File.R that will eventually be parts of my final project to use the source function to read all of my various code instead of my massive R markdown file that I have now.

---
+++ SSEE Project/R scripts/Data_Setup.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+knitr::opts_knit$set(root.dir = "C:/git/SSEE/SSEE Project")
+
+
+setwd("C:/git/SSEE/SSEE Project")
+
+
+galdata=read.csv("./Original Data/GAL settling phenotyping 2024 T3 14.csv")
+
+
+galdata$Treatment <- factor(galdata$Treatment, levels = c("YPD","YPDSET","GAL", "GALSET"))
+
---
+++ SSEE Project/R scripts/Master_File.R
@@ -0,0 +1,6 @@
+#Master Script that will source other .R files 
+
+setwd("C:/git/SSEE/SSEE Project")
+
+source("R scripts/Data_Setup.R")
+
